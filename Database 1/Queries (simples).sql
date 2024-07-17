b7ce5e643cbc48ffc6d49475b7ec57d2a1bb1e44
Óscar Sánchez <oscarsanchezro102@gmail.com>
2024-07-17 21:08:43 +0200

    Update Queries (simples).sql

--- Database 1/Queries (simples).sql
+++ Database 1/Queries (simples).sql
@@ -8,7 +8,7 @@ select NOMPRO, SALPRO from profesores where SALPRO > 1500;
 /* Nombre de los profesores nacidos en el año 1992 */
 select NOMPRO from profesores where FNAPRO like '%92';
 
-/* Nombres de los profesores que ganan más de 1500€ y cuyo oficio es Redes y Software */
+/* Nombre de los profesores que ganan más de 1500€ y cuyo oficio es Redes y Software */
 select NOMPRO from profesores where (SALPRO > 1500) AND (ESPPRO LIKE 'REDES' OR ESPPRO LIKE 'SOFTWARE');
 
 /* Nombre de los profesores que tienen una "u" en la segunda letra de su nombre */
@@ -18,17 +18,17 @@ select NOMPRO from profesores where NOMPRO like '_U%';
 select NOMPRO from profesores where NOMPRO like '%O';
 
 /* TABLE CURSOS */
-/* Títulos de los cursos que tengan dos o más ediciones y que el número de créditos sea mayor de tres */
+/* Título de los cursos que tengan dos o más ediciones y que el número de créditos sea mayor de tres */
 select TITCUR from cursos where EDICUR >= 2 AND CRECUR > 3;
 
-/* Titulos de los cursos cuyo tiempo de desarrollo no exceda de más de 120 días */
+/* Titulo de los cursos cuyo tiempo de desarrollo no exceda de más de 120 días */
 select TITCUR from cursos where (FFICUR - FINCUR) < 120;
 
 /* Título del curso, el literal "comienza" y la fecha de inicio del curso con el formato "lunes 6 de junio, 2016" */
 select TITCUR, LOWER(TO_CHAR(FINCUR,'"COMIENZA EL "DAY D "DE" MONTH", "YYYY')) "FECHAS PREVISTAS DE INICIO" from cursos;
 
 /* TABLE ALUMNOS */
-/* Nombres de los alumnos cuyo tercer dígito de su número de teléfono es 2 por orden alfabético */
+/* Nombre de los alumnos cuyo tercer dígito de su número de teléfono es 2 por orden alfabético */
 select NOMALU from alumnos where TELALU like '__2%' order by NOMALU;
 
 /* Las cuatro primeras letras del nombre de aquellos alumnos que sean de Leganés */
